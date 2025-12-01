7bf9f9d77d7a6a440fac003de1936abf83e026bc
Rikachy <richardlife101@gmail.com>
2025-11-30 22:43:24 -0500

    Add files via upload
    skeleton of the level design

---
+++ MD-HANIF-levels/GamePro.Levels implementation-Skeleton/MyWorld.java
@@ -0,0 +1,35 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(800, 400, 1); 
+        prepare();
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Mc mc = new Mc();
+        addObject(mc,431,296);
+        mc.act();
+        platform platform = new platform();
+        addObject(platform,405,327);
+    }
+}
